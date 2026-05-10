ba730269c6d6ef3c1a1fa85548bc0812d827f549
munir-zaman <munir.zaman2k22@outlook.com>
2026-05-11 00:16:30 +0600

    update modle with more epochs

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ training_data, test_data = mnist.load_data()
 mnist_net = net.NeuralNet([28*28, 15, 10])
 
 def train(modelname : str):
-    mnist_net.SGD(training_data, 30, 3, 10)
+    mnist_net.SGD(training_data, 300, 1.5, 100)
     mnist_net.save_model(modelname)
 
 def test(modelname : str):
     mnist_net.load_model(modelname)
-    mnist_net.evaluate(test_data[:1000])
+    mnist_net.evaluate(test_data)
